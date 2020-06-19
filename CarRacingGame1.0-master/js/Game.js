54829cc2b8b0ed493d57f302d7354b999e8d0c4c
vedantsanghi <64643589+vedantsanghi@users.noreply.github.com>
2020-06-19 17:02:57 +0530

    Add files via upload

---
+++ CarRacingGame1.0-master/js/Game.js
@@ -0,0 +1,76 @@
+class Game {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      player = new Player();
+      var playerCountRef = await database.ref('playerCount').once("value");
+      if(playerCountRef.exists()){
+        playerCount = playerCountRef.val();
+        player.getCount();
+      }
+      form = new Form()
+      form.display();
+    }
+    car1 = createSprite(500, 500, 50, 50)
+    car2 = createSprite(700, 500, 50, 50)
+    car3 = createSprite(900, 500, 50, 50)
+    car4 = createSprite(1100, 500, 50, 50) 
+    cars = [car1, car2, car3, car4]
+  }
+
+  play(){
+    form.hide();
+    textSize(30);
+    text("Game Start", 120, 100)
+    player.getPlayerInfo();
+
+    if(allPlayers !== undefined){
+    //  var display_position = 130;
+     var index = 0
+     var x = 0
+     var y;
+      for(var plr in allPlayers){
+         index = index + 1
+         x = x + 200
+        //  y = displayHeight
+         y = displayHeight - allPlayers[plr].distance;
+      
+        cars[index-1].x = x
+        cars[index-1].y = y
+        if (index===player.index){
+          fill("red")        
+          camera.position.x = displayWidth/2 + 100
+          camera.position.y = cars[index-1].y
+         } /*else {
+          fill("black");
+         }*/
+        //  display_position+=20
+          textSize(15);
+         text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
+        //  push();
+
+        // pop();
+      }
+    }
+
+    if(keyIsDown(UP_ARROW) && player.index !== null){
+      player.distance +=50
+      player.update();
+    }
+  }
+}
